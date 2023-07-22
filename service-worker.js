46837d88e867ed318580528bafffc2d872e01403
customs344243562 <135182456+customs344243562@users.noreply.github.com>
2023-07-22 20:52:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const VERSION = 'version_01';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
-const URLS = [
+const CACHED_URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/main.js`,
@@ -18,43 +18,63 @@ const URLS = [
 ]
 
 const CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('Responding with cache : ' + e.request.url);
-        return request
-      } else {
-        console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
-      }
-    })
-  )
+
+// Open cache on install.
+self.addEventListener('install', event => {
+  event.waitUntil(async function () {
+    const cache = await caches.open(CACHE_NAME)
+
+    await cache.addAll(CACHED_URLS)
+  }())
 })
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
-    })
-  )
+// Cache and update with stale-while-revalidate policy.
+self.addEventListener('fetch', event => {
+  try {
+	  const { request } = event
+
+	  // Prevent Chrome Developer Tools error:
+	  // Failed to execute 'fetch' on 'ServiceWorkerGlobalScope': 'only-if-cached' can be set only with 'same-origin' mode
+	  //
+	  // See also https://stackoverflow.com/a/49719964/1217468
+	  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
+		return
+	  }
+
+	  event.respondWith(async function () {
+		const cache = await caches.open(CACHE_NAME)
+
+		const cachedResponsePromise = await cache.match(request)
+		//~ const networkResponsePromise = fetch(request)
+
+		//~ if (request.url.startsWith(self.location.origin)) {
+		  //~ event.waitUntil(async function () {
+			//~ const networkResponse = await networkResponsePromise
+
+			//~ await cache.put(request, networkResponse.clone())
+		  //~ }())
+		//~ }
+
+		//~ return cachedResponsePromise || networkResponsePromise
+		return cachedResponsePromise
+	  }())
+	}
+	catch {
+		return
+	}
 })
 
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      cacheWhitelist.push(CACHE_NAME);
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
+// Clean up caches other than current.
+self.addEventListener('activate', event => {
+  event.waitUntil(async function () {
+    const cacheNames = await caches.keys()
+
+    await Promise.all(
+      cacheNames.filter((cacheName) => {
+        const deleteThisCache = cacheName !== CACHE_NAME
+
+        return deleteThisCache
+      }).map(cacheName => caches.delete(cacheName))
+    )
+  }())
 })
